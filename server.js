d92c37d8910bf3e09e75c5ba0c9079037f01fb02
richoulondon-wq <richoulondon@gmail.com>
2026-03-10 01:54:59 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+let waitingUser = null;
+
+io.on("connection", socket => {
+
+  socket.on("find", () => {
+
+    if(waitingUser && waitingUser !== socket){
+
+      socket.partner = waitingUser;
+      waitingUser.partner = socket;
+
+      socket.emit("matched");
+      waitingUser.emit("matched");
+
+      waitingUser = null;
+
+    } else {
+
+      waitingUser = socket;
+
+    }
+
+  });
+
+  socket.on("next", () => {
+
+    if(socket.partner){
+      socket.partner.emit("partner-left");
+      socket.partner.partner = null;
+      socket.partner = null;
+    }
+
+  });
+
+  socket.on("offer", data => {
+    if(socket.partner) socket.partner.emit("offer", data);
+  });
+
+  socket.on("answer", data => {
+    if(socket.partner) socket.partner.emit("answer", data);
+  });
+
+  socket.on("ice", data => {
+    if(socket.partner) socket.partner.emit("ice", data);
+  });
+
+  socket.on("disconnect", () => {
+
+    if(waitingUser === socket) waitingUser = null;
+
+    if(socket.partner){
+      socket.partner.emit("partner-left");
+      socket.partner.partner = null;
+    }
+
+  });
+
+});
+
+const PORT = process.env.PORT || 10000;
+
+http.listen(PORT, () => console.log("Server running"));
